723522e5a68d6cdb97fe17ece438619edd76f449
Kim <wdq123550@qq.com>
2025-03-17 18:03:58 +0800

    build 1.0.11

--- Tests/MoneyMakerTests/MoneyMakerTests.swift
+++ Tests/MoneyMakerTests/MoneyMakerTests.swift
@@ -7,13 +7,21 @@ import Testing
 }
 
 
-
-//struct Model: MMAutoCodableColumn {
-//    var phone: String
-//    
-//    enum CodingKeys: String, CodingTableKey {
-//        typealias Root = Model
-//        static let objectRelationalMapping = TableBinding(CodingKeys.self)
-//        case phone
+//eg:
+//private lazy var titleLabel: MMLabel = {
+//    let label = MMLabel(
+//        .font(FontSystem(size: 14, weight: .regular)),
+//        .textColor(.black),
+//        .textAlignment(.left),
+//        .text("标题"),
+//        .bgColor(.clear),
+//        .layerSet(.init(cornerRadius: 10, borderWidth: 20, borderColor: nil)),
+//        .numberOfLine(1),
+//        .lineBreakMode(.byTruncatingTail)
+//    )
+//    self.<#view#>.addSubview(label)
+//    label.snp.makeConstraints { make in
+//        make.center.equalToSuperview()
 //    }
-//}
+//    return label
+//}()
